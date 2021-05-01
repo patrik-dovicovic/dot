38ffc408da81e2a2d0386a9039deabec237a48d0
pa3kdow1c <patrik.dovicovic@gmail.com>
2021-05-01 21:00:03 +0200

    afjodfjklajfkldaj

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
 const path = require('path');
+const cors = require('cors');
+const bp = require('body-parser')
+var nodemailer = require('nodemailer');
 
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true }))
+app.use(cors());
 app.use(express.static(path.join(__dirname, "public")))
 app.listen(80);
 
@@ -9,6 +15,40 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/', 'main.html'));
 });
 
-app.post('/631579451735264526567524327456756415294564527531975164516753157654716151564561567516', (req, res) => {
+app.post('/6', (req, res) => {
+    var x = req.body[0]
     console.log(req.body)
+    var username = x.name
+    var mail = x.email
+    var games = x.game
+    var steam = x.steam_url
+
+
+
+    var transporter = nodemailer.createTransport({
+        service: 'gmail.com',
+        auth: {
+          user: 'dot.esports.webdev@gmail.com',
+          pass: 'pnoe3cWH32IDPdfqB9g9'
+        }
+      });
+      
+      var mailOptions = {
+        from: 'dot.esports.webdev@gmail.com',
+        to: 'dot.esports.webdev@gmail.com',
+        subject: 'new user',
+        text: `|| username = ${username}          
+|| mail = ${mail}         
+|| games = ${games}         
+|| steam = ${steam}         
+`
+      };
+      
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
 });
